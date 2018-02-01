7d30a16872f9df97353b7732d34e39b6d2ec6a00
Eyal Ella <eyal.ella@fundbox.com>
2018-02-01 16:21:22 +0200

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:flowtype/recommended"
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    },
+    sourceType: "module"
+  },
+  plugins: ["react", "flowtype", "prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "prettier/prettier": "error"
+  }
+};
---
+++ src/components/App.js
@@ -0,0 +1,89 @@
+// @flow
+import React, { Component } from "react";
+import { Image } from "react-native";
+import {
+  Container,
+  Header,
+  View,
+  DeckSwiper,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Icon,
+  Button,
+  Footer,
+  FooterTab
+} from "native-base";
+const cards = [
+  {
+    text: "Card One",
+    name: "One",
+    image: require("./img/download.jpeg")
+  },
+  {
+    text: "Card One",
+    name: "One",
+    image: require("./img/download1.jpeg")
+  },
+  {
+    text: "Card One",
+    name: "One",
+    image: require("./img/download2.jpeg")
+  }
+];
+export default class DeckSwiperExample extends Component {
+  state = { active: false };
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <View>
+          <DeckSwiper
+            dataSource={cards}
+            renderItem={item => (
+              <Card style={{ elevation: 3 }}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={item.image} />
+                    <Body>
+                      <Text>{item.text}</Text>
+                      <Text note>NativeBase</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image style={{ height: 300, flex: 1 }} source={item.image} />
+                </CardItem>
+                <CardItem>
+                  <Icon name="heart" style={{ color: "#ED4A6A" }} />
+                  <Text>{item.name}</Text>
+                </CardItem>
+              </Card>
+            )}
+          />
+        </View>
+
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Text>Apps</Text>
+            </Button>
+            <Button>
+              <Text>Camera</Text>
+            </Button>
+            <Button active>
+              <Text>Navigate</Text>
+            </Button>
+            <Button>
+              <Text>Contact</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    );
+  }
+}
